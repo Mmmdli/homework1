b53d472021c0fe196967748884d155d36f88c4d2
Mmmdli <132651256+Mmmdli@users.noreply.github.com>
2023-05-09 23:49:47 -0700

    firstcommit

---
+++ task1/datatypes.js
@@ -0,0 +1,68 @@
+// let num = 15;
+
+// if (num > 0) {
+//     console.log('num musbetdir');
+// } else if (num < 0) {
+//     console.log('num menfidir');
+// } else {
+//     console.log('num sifirdir');
+// }
+
+
+// let n = 6;
+// let fact = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     fact *= i;
+// }
+
+// console.log(fact);
+
+// let ay = 3;
+// let fesil;
+
+// switch(ay) {
+//     case 1:
+//         case 2:
+//             case 12:
+//           fesil = "Qis";
+//           break;
+//           case 3:
+//             case 4:
+//                 case 5:
+//                     fesil = "Yaz";
+//                     break;
+//                     case 6:
+//                         case 7:
+//                             case 8:
+//                                 fesil = "Yay";
+//                                 break;
+//                                 case 9:
+//                                     case 10:
+//                                         case 11:
+//                                             fesil = "Payiz";
+//                                             break;
+//                                             default:
+//                                                 fesil = "bele bir ay yoxdur";
+//                                                 break;
+
+// }
+// console.log(fesil);//"Qis" (3-cu ay Mart,yeni yaz fesline daxil olur)
+
+// let eded = 7
+
+// function mukemmele_yoxla(eded){
+//     let cemi_bolenler = 0;
+//     for (let i = 1; i < eded; i++){
+//         if (eded % i === 0){
+//             cemi_bolenler += i;
+        
+//         }
+//     }
+//     if (cemi_bolenler === eded) {
+//         console.log(eded + 'mukemmeldir.');
+//     } else {
+//         console.log(eded + 'mukemmel deyil.');
+//     }
+// }
+// mukemmele_yoxla(eded)
